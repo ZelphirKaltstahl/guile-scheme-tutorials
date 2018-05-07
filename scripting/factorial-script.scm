fc415d6d85ffc2208a33173e23f0e6db28c19b4e
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2018-05-07 07:44:35 +0200

    detailed comment

--- scripting/factorial-script.scm
+++ scripting/factorial-script.scm
@@ -1,6 +1,15 @@
 #!/usr/local/bin/guile \
 -e main -s
 !#
+
+;; How to run this program?
+;; Example:
+;; guile -e main -s factorial-script.scm 50
+;; Explanation:
+;; -e specifies the procedure to run
+;; -s specifies to run this as a script
+;; 50 is the number we take as input to the script
+
 (define (fact n)
   (if (zero? n) 1
       (* n (fact (- n 1)))))
@@ -8,8 +17,3 @@
 (define (main args)
   (display (fact (string->number (cadr args))))
   (newline))
-
-;; guile -e main -s factorial-script.scm 50
-;; -e specifies the procedure to run
-;; -s specifies to run this as a script
-;; 50 is the number we take as input to the script
