1420e6a6c83fbc842af135b0a6065004fc780028
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2018-05-09 13:14:58 +0200

    tutorial does not work entirely, somehow fact is not loaded correctly from fact.scm

--- scripting/modules.scm
+++ scripting/modules.scm
@@ -1,16 +1,27 @@
-#!/usr/local/bin/guile \
--l fact -e (@ (fac) main) -s
+#!/usr/bin/env sh
+exec guile -l fact.scm -e '(@ (my-module) main)' -s "$0" "$@"
 !#
 
 ;; Explanation:
-;; -e (@ (fac) main)
-;; If run as a script run the `fac` module's `main`.
+;; -e (my-module)
+;; If run as a script run the `my-module` module's `main`.
 ;; (Use `@@` to reference not exported procedures.)
 ;; -s
 ;; Run the script.
 
-(define-module (fac)
+(define-module (my-module)
   #:export (main))
 
 ;; Create a module named `fac`.
 ;; Export the `main` procedure as part of `fac`.
+
+(define (n-choose-k n k)
+  (/ (fact n)
+     (* (fact k)
+        (fact (- n k)))))
+
+(define (main args)
+  (let ((n (string->number (cadr args)))
+        (k (string->number (caddr args))))
+    (display (n-choose-k n k))
+    (newline)))
